66abc4be9b429683e446ddf67a456603f8ab557e
John Michael <johnfrontend@gmail.com>
2022-12-06 16:45:04 -0300

    Criando os Commands

---
+++ Domain/Commands/Results/GerarCartaoVirtualCommandResult.cs
@@ -0,0 +1,8 @@
+namespace Domain.Commands.Results
+{
+    public class GerarCartaoVirtualCommandResult
+    {
+        public string Email { get; set; }
+        public string NumeroCartao { get; set; }
+    }
+}
