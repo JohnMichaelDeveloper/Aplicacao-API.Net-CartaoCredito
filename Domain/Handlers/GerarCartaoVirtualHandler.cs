fd9b227148d227b0ed288cb81274d49741f2ca5d
John Michael <johnfrontend@gmail.com>
2022-12-06 17:21:55 -0300

    Criando os testes

---
+++ Domain/Handlers/GerarCartaoVirtualHandler.cs
@@ -0,0 +1,20 @@
+using Domain.Commands.Inputs;
+using Domain.Commands.Results;
+
+namespace Domain.Handlers
+{
+
+	public class GerarCartaoVirtualHandler
+	{
+		public GerarCartaoVirtualCommandResult Handle(GerarCartaoVirtualCommand command)
+		{
+			//Validar Command
+
+			//Gerar número de cartão aleatório
+
+			//Criar command result e retornar
+
+			return null;
+		}
+	}
+}
